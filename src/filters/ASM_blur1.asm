fba345215384ec903168189cc55639b4aef2ebaf
Arribas Joaquin Manuel <jarribas@ws2.labo7>
2015-04-23 20:15:43 -0300

    terminado con segmentation fault

--- src/filters/ASM_blur1.asm
+++ src/filters/ASM_blur1.asm
@@ -13,7 +13,9 @@ global ASM_blur1
 %define j  [rbp-8]  ; defino etiqueta a variable local, utilizada para recorrer la matriz 
 %define i  [rbp-16] ; defino etiqueta a variable local, utilizada para recorrer la matriz  
 %define k  [rbp-24] ; defino etiqueta a variable local, utilizada para recorrer el byte 
+nueve: dd 0x00090009
 
+extern malloc
 
 
 ASM_blur1:
@@ -67,15 +69,15 @@ ASM_blur1:
   cmp r12, 2 ; me fijo si tengo dos filas
   je .fin ; si es asi no tengo nada que hacer
 
+  mov rax, 0
 
   mov ecx, ebx ; guardo en rcx la cantidad de elementos en una fila
   .cargoPrimerFila:
     ; en r14 tengo el primer vector
     ; en r13 tengo el puntero a mi imagen
-    mov rax, i
-    movd edx, [r13 + rax * OFFSET_DATO] ; guardo en edx el pixel = 4 BYTES
-    movd [r14 + rax * OFFSET_DATO], edx ; guardo en el vector el pixel copiado de la imagen
-    inc qword i ; me muevo una columna
+    mov edx, [r13 + rax * OFFSET_DATO] ; guardo en edx el pixel = 4 BYTES
+    mov [r14 + rax * OFFSET_DATO], edx ; guardo en el vector el pixel copiado de la imagen
+    inc rax ; me muevo una columna
     loop .cargoPrimerFila
 
   mov esi, ebx ; guardo en esi la cantidad de columas
@@ -85,6 +87,8 @@ ASM_blur1:
   dec ecx ; tengo h-2 iteraciones
   mov qword j, 1 ; contador de elementos columna en 1
   .ciclo:
+    cmp ecx, 0
+    je .fin
     mov rdx, r15 ; guardo en rdx (temp) mi vector 0
     mov r15, r14 ; guardo en r15 (vector 0) r14 (vector 1)
     mov r14, rdx ; guardo en r14 (vector 1) mi vector 0
@@ -94,39 +98,137 @@ ASM_blur1:
       cmp ebx, i ; me fijo si ya recorri todos los elementos de la fila
       je .promediar
       mov rax, i ; guardo en rax mi indice
-      movd edx, [r13 + rax * OFFSET_DATO] ; guardo en edx el pixel = 4 BYTES
-      movd [r14 + rax * OFFSET_DATO], edx ; guardo en el vector el pixel copiado de la imagen
+      mov edx, [r13 + rax * OFFSET_DATO] ; guardo en edx el pixel = 4 BYTES
+      mov [r14 + rax * OFFSET_DATO], edx ; guardo en el vector el pixel copiado de la imagen
       inc qword i ; me muevo una columna
       jmp .cargarSiguienteVector
 
     .promediar:
       movdqu xmm0, [r15] ; guardo en xmm0 4 pixeles del vector 0
       movdqu xmm1, [r14] ; guardo en xmm1 4 pixeles del vector 1
-      inc j 
+      inc qword j 
       mov rax, j ; guardo en rax mi indice
       movdqu xmm2, [r13 + rax * OFFSET_DATO] ; guardo en xmm2 4 pixeles de la tercer fila
       ; desempaquetar, sumar componente a componente, empaquetar
-      ; xmmo = m0p3 | m0p2 | m0p1 | m0p0  
-      ; xmm1 = m1p3 | m1p2 | m1p1 | m1p0  
-      ; xmm2 = m2p3 | m2p2 | m2p1 | m2p0  
+      ; xmmo = 0p3 | 0p2 | 0p1 | 0p0  
+      ; xmm1 = 1p3 | 1p2 | 1p1 | 1p0  
+      ; xmm2 = 2p3 | 2p2 | 2p1 | 2p0  
       ; agarrar todos los px0, pasarlos registros 128, sumarlos, hasta px3
       pxor xmm7, xmm7 ; xmm7 = 0
       movdqu xmm8, xmm0 ; xmm8 va a tener la parte alta de xmm0
-      punpcklwd xmm0, xmm7 ; intercalo ceros con px0 y px1 | 0 | px1h | 0 | px1l | 0 | px0h | 0 | px0l 
-      punpckhwd xmm8, xmm7 ; intercalo ceros con px2 y px3 | 0 | px3h | 0 | px3l | 0 | px2h | 0 | px2l
-      ; packuswb x
-      punpcklwd xmm0, xmm1 ; | m1p1H | m0p1H | m1p1L | m0p1L | m1p0H | m0p0H | m1p0L | m0p0L
+      pslldq xmm8, 4 ; xmm8 =  | px2 | px1 | px0 | 0 | 
+      psrldq xmm8, 12 ; xmm8 = |  0  |  0  |  0  |px2|
+      punpcklbw xmm0, xmm7 ; xmm0 = | 0 | A1 | 0 | B1 | 0 | G1 | 0 | R1 | 0 | A0 | 0 | B0 | 0 | G0 | 0 | R0 |
+      punpcklbw xmm8, xmm7 ; xmm8 = | 0 | 0  | 0 | 0  | 0 | 0  | 0 | 0  | 0 | A2 | 0 | B2 | 0 | G2 | 0 | R2 |
+      
+      paddw xmm0, xmm8 ; xmm0 = | 0 | A1 | 0 | B1 | 0 | G1 | 0 | R1 | 0 | A0+A2 | 0 | B0+B2 | 0 | G0+G2 | 0 | R0+R2 |
+
+      movdqu xmm8, xmm0 ; xmm8 = | 0 | A1 | 0 | B1 | 0 | G1 | 0 | R1 | 0 | A0+A2 | 0 | B0+B2 | 0 | G0+G2 | 0 | R0+R2 |
+
+      psrldq xmm8, 8 ; xmm8 = | 0 | 0 | 0 | 0 | 0 | 0  | 0 | 0  | 0 | A1 | 0 | B1 | 0 | G1 | 0 | R1 |
+
+      paddw xmm0, xmm8 ; xmm0 = | 0 | 0 | 0 | 0  | 0 | 0  | 0 | 0  | 0 | A0+A1+A2 | 0 | B0+B1+B2 | 0 | G0+G1+G2 | 0 | R0+R1+R2 |
+
+      packuswb xmm0, xmm7 ; xmm0 = | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | A0+A1+A2 | B0+B1+B2 | G0+G1+G2 | R0+R1+R2 |
 
 
+      pxor xmm7, xmm7 ; xmm7 = 0
+      movdqu xmm8, xmm1 ; xmm8 va a tener la parte alta de xmm0
+      pslldq xmm8, 4 ; xmm8 =  | px2 | px1 | px0 | 0 | 
+      psrldq xmm8, 12 ; xmm8 = |  0  |  0  |  0  |px2|
+      punpcklbw xmm1, xmm7 ; xmm1 = | 0 | A1 | 0 | B1 | 0 | G1 | 0 | R1 | 0 | A0 | 0 | B0 | 0 | G0 | 0 | R0 |
+      punpcklbw xmm8, xmm7 ; xmm8 = | 0 | 0  | 0 | 0  | 0 | 0  | 0 | 0  | 0 | A2 | 0 | B2 | 0 | G2 | 0 | R2 |
+      
+      paddw xmm1, xmm8 ; xmm1 = | 0 | A1 | 0 | B1 | 0 | G1 | 0 | R1 | 0 | A0+A2 | 0 | B0+B2 | 0 | G0+G2 | 0 | R0+R2 |
+
+      movdqu xmm8, xmm1 ; xmm8 = | 0 | A1 | 0 | B1 | 0 | G1 | 0 | R1 | 0 | A0+A2 | 0 | B0+B2 | 0 | G0+G2 | 0 | R0+R2 |
+
+      psrldq xmm8, 8 ; xmm8 = | 0 | 0 | 0 | 0 | 0 | 0  | 0 | 0  | 0 | A1 | 0 | B1 | 0 | G1 | 0 | R1 |
+
+      paddw xmm1, xmm8 ; xmm1 = | 0 | 0 | 0 | 0  | 0 | 0  | 0 | 0  | 0 | A0+A1+A2 | 0 | B0+B1+B2 | 0 | G0+G1+G2 | 0 | R0+R1+R2 |
+
+      packuswb xmm1, xmm7 ; xmm1 = | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | A0+A1+A2 | B0+B1+B2 | G0+G1+G2 | R0+R1+R2 |
+      
+
+      pxor xmm7, xmm7 ; xmm7 = 0
+      movdqu xmm8, xmm2 ; xmm8 va a tener la parte alta de xmm0
+      pslldq xmm8, 4 ; xmm8 =  | px2 | px1 | px0 | 0 | 
+      psrldq xmm8, 12 ; xmm8 = |  0  |  0  |  0  |px2|
+      punpcklbw xmm2, xmm7 ; xmm2 = | 0 | A1 | 0 | B1 | 0 | G1 | 0 | R1 | 0 | A0 | 0 | B0 | 0 | G0 | 0 | R0 |
+      punpcklbw xmm8, xmm7 ; xmm8 = | 0 | 0  | 0 | 0  | 0 | 0  | 0 | 0  | 0 | A2 | 0 | B2 | 0 | G2 | 0 | R2 |
+      
+      paddw xmm2, xmm8 ; xmm2 = | 0 | A1 | 0 | B1 | 0 | G1 | 0 | R1 | 0 | A0+A2 | 0 | B0+B2 | 0 | G0+G2 | 0 | R0+R2 |
+
+      movdqu xmm8, xmm2 ; xmm8 = | 0 | A1 | 0 | B1 | 0 | G1 | 0 | R1 | 0 | A0+A2 | 0 | B0+B2 | 0 | G0+G2 | 0 | R0+R2 |
+
+      psrldq xmm8, 8 ; xmm8 = | 0 | 0 | 0 | 0 | 0 | 0  | 0 | 0  | 0 | A1 | 0 | B1 | 0 | G1 | 0 | R1 |
+
+      paddw xmm2, xmm8 ; xmm2 = | 0 | 0 | 0 | 0  | 0 | 0  | 0 | 0  | 0 | A0+A1+A2 | 0 | B0+B1+B2 | 0 | G0+G1+G2 | 0 | R0+R1+R2 |
+
+      packuswb xmm2, xmm7 ; xmm2 = | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | A0+A1+A2 | B0+B1+B2 | G0+G1+G2 | R0+R1+R2 |
 
 
+      punpcklbw xmm0, xmm7 ; xmm0 = suma de los tres pixeles de la primer fila intercalados con ceros 
+      
+      punpcklbw xmm1, xmm7 ; xmm1 = suma de los tres pixeles de la segunda fila intercalados con ceros 
+
+      punpcklbw xmm2, xmm7 ; xmm2 = suma de los tres pixeles de la tercer fila intercalados con ceros 
+
+      paddw xmm1, xmm2
+      paddw xmm0, xmm1
+
+      ; xmm0 = 0 | 0 | 0 | 0 | sumA | sumB | sumG | sumR |
+      ;xmm0 tiene la suma de los 9 pixeles
+
+      movdqu xmm9, xmm0
+      
+      pxor xmm5, xmm5
+      pxor xmm6, xmm6 
+      pxor xmm7, xmm7
+      cvtdq2pd xmm5, xmm9 ; xmm5 = sumG | sumR
+
+      psrldq xmm9, 4 ; xmm9 = 0 | 0 | 0 | 0 | 0 | 0 | sumA | sumB
+
+      cvtdq2pd xmm6, xmm9 ; xmm6 = sumA | sumB
+
+      mov r8d, nueve 
+
+      movd xmm4, r8d ; xmm4 = 0 | 0 | 0 | 0 | 0 | 0 | 9 | 9 |
+
+      cvtdq2pd xmm7, xmm4 ; xmm7 = 9 | 9  
+
+      divpd xmm6, xmm7 ; xmm6 = sumA/9 | sumB/9
+
+      divpd xmm5, xmm7 ; xmm5 = sumG/9 | sumR/9
+
+      pxor xmm10, xmm10
 
 
+      cvtpd2dq xmm10, xmm6 ; xmm10 = 0 | 0 | 0 | 0 | sumA/9 | sumB/9
+
+      pslldq xmm10, 8
+
+      cvtpd2dq xmm10, xmm6 ; xmm10 = | sumA/9 | sumB/9 | sumG/9 | sumR/9 |
+
+  
+
+      packusdw xmm10, xmm7 ; xmm0 tiene en los primeros 32 bits la suma de los 9 pixeles = | 0 | 0 | 0 | pixel |
+      packuswb xmm10, xmm7 ; xmm0 tiene en los primeros 32 bits la suma de los 9 pixeles = | 0 | 0 | 0 | pixel |
 
 
+      movd r8d, xmm10 ; guardo el pixel en 32 bits
+
+      dec rax
+      mov [r13 + rax * OFFSET_DATO], r8d ; guardo el pixel
+      dec ecx
+      jmp .ciclo
 
 
-
-
-
-  ret
+  .fin:
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop rbx
+    add rsp, 24
+    ret
