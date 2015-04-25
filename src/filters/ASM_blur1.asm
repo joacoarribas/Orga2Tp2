bf7c3db8ad8e725af177a0ab73a77b60fe1fafb9
Arribas Joaquin Manuel <jarribas@ws1.labo6>
2015-04-25 20:51:41 -0300

    no tira error por lo menos el blur 1

--- src/filters/ASM_blur1.asm
+++ src/filters/ASM_blur1.asm
@@ -9,51 +9,48 @@
 global ASM_blur1
 
 %define OFFSET_DATO 4
-
-%define j  [rbp-8]  ; defino etiqueta a variable local, utilizada para recorrer la matriz 
-%define i  [rbp-16] ; defino etiqueta a variable local, utilizada para recorrer la matriz  
-%define k  [rbp-24] ; defino etiqueta a variable local, utilizada para recorrer el byte 
-nueve: dd 0x00090009
-
 extern malloc
 
+section .rodata
+  nueve: dd 0x00090009
+
+section .text
 
 ASM_blur1:
   ; armo el stack frame
   push rbp
   mov rbp, rsp
-  sub rsp, 24 ; reservo espacio para las variables locales
   push rbx
   push r12
   push r13 
   push r14 
-  push r15 ; la pila esta alineada por ahora
+  push r15 ; la pila esta desalineada por ahora
 
   ; codigo
-  ; rdi tengo el width, cantidad de elementos por fila
-  ; rsi tengo el heigth, cantidad de elementos por columna 
-  ; rcx tengo la matriz
+  ; edi tengo el width, cantidad de elementos por fila
+  ; esi tengo el heigth, cantidad de elementos por columna 
+  ; rdx tengo la matriz
 
-  ; xor rbx, rbx ; limpio rbx
   mov ebx, edi ; guardo en rbx la cantidad de elementos por fila
-  ; xor r12, r12 ; limpio r12
   mov r12d, esi ; guardo en r12 la cantidad de elementos por columna
-  mov r13, rcx ; guardo en r14 el puntero a mi imagen
-
-  mov qword j, 0 ; inicializo j (contador fila) en cero 
-  mov qword i, 0 ; inicializo i (contador columna) en cero 
-  mov qword k, 0 ; inicializo k en cero 
+  mov r13, rdx ; guardo en r14 el puntero a mi imagen
 
   mov r15, 0 ; dejo r15 en cero 
   lea rdi, [r15 + rbx * OFFSET_DATO] ; guardo en rdi 0+cantidad de elementos por fila * 4
+
+  sub rsp, 8 ; alineo la pila
   call malloc ; creo un vector con tamaño igual a la fila de la matriz
+  add rsp, 8 ; desalineo la pila
   ; tengo en rax el vector
 
   mov r14, rax ; guardo en r14 el primer vector
 
   mov r15, 0 ; dejo r15 en cero 
   lea rdi, [r15 + rbx * OFFSET_DATO] ; guardo en rdi 0+cantidad de elementos por fila * 4
+
+  sub rsp, 8 ; alineo la pila
   call malloc ; creo un vector con tamaño igual a la fila de la matriz
+  add rsp, 8 ; desalineo la pila
   ; tengo en rax el vector
 
   mov r15, rax ; guardo en r15 el segundo vector 
@@ -69,46 +66,54 @@ ASM_blur1:
   cmp r12, 2 ; me fijo si tengo dos filas
   je .fin ; si es asi no tengo nada que hacer
 
-  mov rax, 0
 
-  mov ecx, ebx ; guardo en rcx la cantidad de elementos en una fila
+  mov rax, 0
   .cargoPrimerFila:
+    cmp rax, rbx
+    je .sigo
     ; en r14 tengo el primer vector
     ; en r13 tengo el puntero a mi imagen
     mov edx, [r13 + rax * OFFSET_DATO] ; guardo en edx el pixel = 4 BYTES
-    mov [r14 + rax * OFFSET_DATO], edx ; guardo en el vector el pixel copiado de la imagen
+    lea r8, [r14 + rax*OFFSET_DATO]
+    mov [r8], edx ; guardo en el vector el pixel copiado de la imagen
     inc rax ; me muevo una columna
-    loop .cargoPrimerFila
+    jmp .cargoPrimerFila
 
-  mov esi, ebx ; guardo en esi la cantidad de columas
-  dec esi ; voy a recorrer salteand el primer y ultimo elemento 
+  .sigo:  
 
-  mov ecx, r12d ; muevo a rcx la cantidad de filas que tengo
-  dec ecx ; tengo h-2 iteraciones
-  mov qword j, 1 ; contador de elementos columna en 1
+  lea rdi, [rbx - 4] ; me muevo hasta la columna h-2
+
+  lea rsi, [r12 - 2] ; tengo h-2 iteraciones
+  mov r8, 1 ; contador de elementos columna en 1
   .ciclo:
-    cmp ecx, 0
+    cmp rsi, 0
     je .fin
     mov rdx, r15 ; guardo en rdx (temp) mi vector 0
     mov r15, r14 ; guardo en r15 (vector 0) r14 (vector 1)
     mov r14, rdx ; guardo en r14 (vector 1) mi vector 0
-    mov qword i, 0 ; limpio mi contador i para recorrer filas
+    mov rax, 0 ; uso rax como mi indice
+    lea r13, [r13 + rbx * OFFSET_DATO] ; me paro en la segunda fila 
     
     .cargarSiguienteVector:
-      cmp ebx, i ; me fijo si ya recorri todos los elementos de la fila
-      je .promediar
-      mov rax, i ; guardo en rax mi indice
+      cmp ebx, eax ; me fijo si ya recorri todos los elementos de la fila
+      je .prom
       mov edx, [r13 + rax * OFFSET_DATO] ; guardo en edx el pixel = 4 BYTES
       mov [r14 + rax * OFFSET_DATO], edx ; guardo en el vector el pixel copiado de la imagen
-      inc qword i ; me muevo una columna
+      inc rax ; me muevo una columna
       jmp .cargarSiguienteVector
 
+    .prom:
+      mov rax, 0
+      mov r9, r13 ; r9 apunta a mi segunda fila de la matriz
+      lea r9, [r9 + rbx * OFFSET_DATO] ; r9 apunta a la siguiente fila
+
     .promediar:
-      movdqu xmm0, [r15] ; guardo en xmm0 4 pixeles del vector 0
-      movdqu xmm1, [r14] ; guardo en xmm1 4 pixeles del vector 1
-      inc qword j 
-      mov rax, j ; guardo en rax mi indice
-      movdqu xmm2, [r13 + rax * OFFSET_DATO] ; guardo en xmm2 4 pixeles de la tercer fila
+      cmp rax, rdi 
+      je .decremento
+      movdqu xmm0, [r15 + rax * OFFSET_DATO] ; guardo en xmm0 4 pixeles del vector 0
+      movdqu xmm1, [r14 + rax * OFFSET_DATO] ; guardo en xmm1 4 pixeles del vector 1
+
+      movdqu xmm2, [r9 + rax * OFFSET_DATO] ; guardo en xmm2 4 pixeles de la tercer fila
       ; desempaquetar, sumar componente a componente, empaquetar
       ; xmmo = 0p3 | 0p2 | 0p1 | 0p0  
       ; xmm1 = 1p3 | 1p2 | 1p1 | 1p0  
@@ -129,8 +134,6 @@ ASM_blur1:
 
       paddw xmm0, xmm8 ; xmm0 = | 0 | 0 | 0 | 0  | 0 | 0  | 0 | 0  | 0 | A0+A1+A2 | 0 | B0+B1+B2 | 0 | G0+G1+G2 | 0 | R0+R1+R2 |
 
-      ; packuswb xmm0, xmm7 ; xmm0 = | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | A0+A1+A2 | B0+B1+B2 | G0+G1+G2 | R0+R1+R2 |
-
 
       pxor xmm7, xmm7 ; xmm7 = 0
       movdqu xmm8, xmm1 ; xmm8 va a tener la parte alta de xmm0
@@ -147,8 +150,6 @@ ASM_blur1:
 
       paddw xmm1, xmm8 ; xmm1 = | 0 | 0 | 0 | 0  | 0 | 0  | 0 | 0  | 0 | A0+A1+A2 | 0 | B0+B1+B2 | 0 | G0+G1+G2 | 0 | R0+R1+R2 |
 
-      ; packuswb xmm1, xmm7 ; xmm1 = | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | A0+A1+A2 | B0+B1+B2 | G0+G1+G2 | R0+R1+R2 |
-      
 
       pxor xmm7, xmm7 ; xmm7 = 0
       movdqu xmm8, xmm2 ; xmm8 va a tener la parte alta de xmm0
@@ -164,15 +165,6 @@ ASM_blur1:
       psrldq xmm8, 8 ; xmm8 = | 0 | 0 | 0 | 0 | 0 | 0  | 0 | 0  | 0 | A1 | 0 | B1 | 0 | G1 | 0 | R1 |
 
       paddw xmm2, xmm8 ; xmm2 = | 0 | 0 | 0 | 0  | 0 | 0  | 0 | 0  | 0 | A0+A1+A2 | 0 | B0+B1+B2 | 0 | G0+G1+G2 | 0 | R0+R1+R2 |
-
-      ; packuswb xmm2, xmm7 ; xmm2 = | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | A0+A1+A2 | B0+B1+B2 | G0+G1+G2 | R0+R1+R2 |
-
-
-      ;punpcklbw xmm0, xmm7 ; xmm0 = suma de los tres pixeles de la primer fila intercalados con ceros 
-      
-      ;punpcklbw xmm1, xmm7 ; xmm1 = suma de los tres pixeles de la segunda fila intercalados con ceros 
-
-      ;punpcklbw xmm2, xmm7 ; xmm2 = suma de los tres pixeles de la tercer fila intercalados con ceros 
 
       paddw xmm1, xmm2
       paddw xmm0, xmm1
@@ -215,11 +207,13 @@ ASM_blur1:
 
       movd r8d, xmm10 ; guardo el pixel en 32 bits
 
-      dec rax
+      inc rax
       mov [r13 + rax * OFFSET_DATO], r8d ; guardo el pixel
-      dec ecx
-      jmp .ciclo
+      jmp .promediar
 
+    .decremento:
+      dec rsi
+      jmp .ciclo
 
   .fin:
     pop r15
@@ -227,5 +221,5 @@ ASM_blur1:
     pop r13
     pop r12
     pop rbx
-    add rsp, 24
+    pop rbp
     ret
