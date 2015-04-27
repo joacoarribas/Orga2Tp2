7ddad95fe1149b424fc4d88c3967ecf9315a3142
Arribas Joaquin Manuel <jarribas@ws21.labo7>
2015-04-27 10:24:25 -0300

    caso borde de blur1

--- src/filters/ASM_merge2.asm
+++ src/filters/ASM_merge2.asm
@@ -8,4 +8,20 @@
 ; void ASM_merge2(uint32_t w, uint32_t h, uint8_t* data1, uint8_t* data2, float value)
 global ASM_merge2
 ASM_merge2:
+  push rbp
+  mov rbp, rsp
+  push rbx
+  push r12
+  push r13
+  push r14
+  push r15 ; la pila esta desalineada
+
+  ;codigo
+
+  pop r15
+  pop r14
+  pop r13
+  pop r12
+  pop rbx
+  pop rbp
   ret
